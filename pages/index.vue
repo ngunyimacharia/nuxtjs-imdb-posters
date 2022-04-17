11429202a1bd48c84643ac7aaeba7dffd87e2190
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2022-04-17 23:32:33 +0200

    Generate using TMDB instead of IMDB

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="bg-gray-100 min-h-screen min-w-screen">
+
     <div class="max-w-3xl mx-auto py-12 sm:px-6 lg:px-8">
       <form @submit.prevent="search" class="bg-white shadow sm:rounded-lg">
         <div class="px-4 py-5 sm:p-6">
@@ -17,15 +18,31 @@
         </div>
         <div class="text-center p-2">
           <button
+            v-if="!loading"
             type="submit"
             class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            Search
+            {{searchQuery ? "Search" : "Random"}}
           </button>
+          <p v-if="loading" class="m-2 italic text-gray-400">
+            {{loading}}
+          </p>
         </div>
       </form>
     </div>
-    {{show}}
+
+    <div class="text-center">
+      <img
+        :alt="`${show.title} poster`"
+        v-if="show"
+        class="mx-auto my-10 w-200 h-200"
+        :src="posterURL"
+        referrerpolicy="no-referrer"
+        @load="loading = null"
+        @error="search"
+      />
+    </div>
+
   </div>
 </template>
 
@@ -33,39 +50,86 @@
 export default {
   data(){
     return {
+      loading:false,
       searchQuery: null,
-      show:null
+      show:null,
     };
+  },
+  mounted(){
+    this.search();
+  },
+  computed:{
+    posterURL(){
+      if(!this.show){
+        return;
+      }
+      return this.$cloudinary.image.fetchRemote(
+        `https://image.tmdb.org/t/p/original/${this.show.backdrop_path}`,
+        {
+          width:800,
+          height:800,
+          crop:"fit",
+          transformation: [
+            // {
+            //   color:"#" + Math.floor(Math.random()*16777215).toString(16),
+            //   overlay: {
+            //     font_family: "Times",
+            //     font_size: 90,
+            //     text: this.show.release_date
+            //   }
+            // },
+            {
+              color:"#" + Math.floor(Math.random()*16777215).toString(16),
+              overlay: {
+                font_family: "Ultra",
+                font_weight: "bold",
+                font_size: 120,
+                text: this.show.title
+              }
+            },
+            {flags: "layer_apply", gravity: "south", y: 100}
+          ]
+        }
+      );
+    }
+  },
+  watch:{
+    show(val){
+      console.log(val);
+    }
   },
   methods:{
     search(){
+      Array.prototype.random = function () {
+        return this[Math.floor((Math.random()*this.length))];
+      }
+
+      this.loading = "Getting search results...";
       const requestOptions ={
         method: 'GET',
         redirect: 'follow'
       };
 
-      const requestURL = `https://imdb-api.com/en/API/Search/${process.env.NUXT_ENV_IMDB_API_KEY}/${this.searchQuery}`;
+      const requestURL = "https://api.themoviedb.org/3/search/movie?";
 
-      fetch(requestURL, requestOptions)
+      const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+      const queryParams = new URLSearchParams({
+          api_key: process.env.NUXT_ENV_TMDB_API_KEY,
+          query: this.searchQuery ?? alphabet[Math.floor(Math.random() * alphabet.length)],
+      });
+
+      fetch(requestURL + queryParams, requestOptions)
         .then(response => response.json())
-        .then(response => this.getShowDetails(response.results[0]))
-        .catch(error => alert('error', error));
+        .then(
+          response => this.show = response
+                                        .results
+                                        .filter(show => show.backdrop_path)
+                                        .random()
+            )
+          .then(() => this.loading="Loading image..")
+        .catch(error => alert('Error:' + error));
     },
-    getShowDetails(show){
-
-      const requestOptions ={
-        method: 'GET',
-        redirect: 'follow'
-      };
-
-      const requestURL = `https://imdb-api.com/en/API/Title/${process.env.NUXT_ENV_IMDB_API_KEY}/${show.id}/Images,`;
-
-      fetch(requestURL, requestOptions)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .then(response => this.show = response)
-        .catch(error => alert('error', error));
-    }
   }
 }
 </script>
